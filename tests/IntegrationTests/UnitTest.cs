2e0f5bfa6a9f5dd545389069109b74709c2cd666
Victor Molostov <86685532+molostovvs@users.noreply.github.com>
2025-04-20 16:37:00 +0300

    initial commit

---
+++ tests/IntegrationTests/UnitTest.cs
@@ -0,0 +1,15 @@
+﻿namespace IntegrationTests;
+
+public class UnitTest
+{
+    [Fact]
+    public void Success()
+    {
+    }
+
+    [Fact]
+    public void Fail()
+    {
+        Assert.Fail();
+    }
+}
---
+++ tests/UnitTests/Tests.cs
@@ -0,0 +1,16 @@
+﻿namespace UnitTests;
+
+public class Tests
+{
+    [Test]
+    public async Task Success()
+    {
+        await Task.CompletedTask;
+    }
+
+    [Test]
+    public void Fail()
+    {
+        Assert.Fail("");
+    }
+}
